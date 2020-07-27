d06edfa00620ebef8a5f381a08a3734eca13bd4d
Naomi Hariman <nhariman@f1r1s6.codam.nl>
2020-07-27 22:44:11 +0200

    working on phonebook class

---
+++ ex01/ClassPhonebook.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ClassPhonebook.hpp                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/07/27 22:27:27 by nhariman      #+#    #+#                 */
+/*   Updated: 2020/07/27 22:42:49 by nhariman      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLASSPHONEBOOK_H
+#define CLASSPHONEBOOK_H
+
+#include <iostream>
+
+class phonebook
+{
+private:
+	std::string input;
+
+
+public:
+	phonebook(void);
+	void initialise_phonebook(void);
+	
+
+};
+
+#endif
