6ca5eff62d071e4c40cf007e43977417f2d838ed
Niks Hariman <nhariman@hotmail.com>
2021-11-24 16:53:01 +0100

    updated ex06 main

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/02 14:33:54 by nhariman      #+#    #+#                 */
-/*   Updated: 2021/11/03 20:50:33 by nhariman      ########   odam.nl         */
+/*   Updated: 2021/11/24 16:52:06 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ int		main(int ac, char** av)
 {
 	Karen	carl;
 	const std::string	levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
-	std::string	complaint;
-
 	if (ac != 2)
 	{
 		if (ac == 1)
@@ -33,7 +31,7 @@ int		main(int ac, char** av)
 		case (2):
 		case (3):{
 			
-			for (int i = case_study; i >=0; i--)
+			for (int i = case_study; i >= 0; i--)
 			{
 				std::cout << "[" << levels[i] << "]" << std::endl;
 				carl.complain(levels[i]);
@@ -46,6 +44,5 @@ int		main(int ac, char** av)
 			std::cout << "[Nothing to report!]" << std::endl;
 			break ;
 	}
-
 	return (0);
 }
