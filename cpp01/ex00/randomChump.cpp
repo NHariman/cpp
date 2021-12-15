9ef4c2c5ee689cda102be29a8b668147170698d6
Niks Hariman <nhariman@hotmail.com>
2021-12-15 20:41:21 +0100

    changed random chump name ot obj

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -6,7 +6,7 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/01 14:19:40 by nhariman      #+#    #+#                 */
-/*   Updated: 2021/12/08 19:17:50 by nhariman      ########   odam.nl         */
+/*   Updated: 2021/12/15 20:27:35 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void		randomChump(std::string name)
 {
-	Zombie	fred(name); // name it
-	fred.announce(); // announce its presence
+	Zombie	obj(name); // name it
+	obj.announce(); // announce its presence
 }
