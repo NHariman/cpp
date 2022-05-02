d4d25a05532562627bb5ce5fac408fea0aaa5fe8
Niks Hariman <nhariman@hotmail.com>
2022-05-02 20:04:51 +0200

    finished array

--- cpp07/ex02/inc/Array.hpp
+++ cpp07/ex02/inc/Array.hpp
@@ -6,35 +6,85 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/14 15:18:41 by nhariman      #+#    #+#                 */
-/*   Updated: 2022/04/14 20:54:54 by nhariman      ########   odam.nl         */
+/*   Updated: 2022/05/02 19:58:04 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP // can also use pragma once
 
+#include "colours.hpp"
 /*
 	https://www.programiz.com/cpp-programming/class-templates
 	https://www.geeksforgeeks.org/templates-cpp/ 
 	on class templates
+
+	https://www.tutorialspoint.com/cplusplus/subscripting_operator_overloading.htm
+	on subscript operator overloading
 */
 
 template <class T>
 class Array{
+		private:
+			unsigned int	_len;
 	public:
 		T*	_array;
-		Array()
+		Array() : _len(10)
 		{
-			
+			std::cout << B_BLUEB << "Created an array of 10" << B_END << std::endl;
+			_array = new T[10]; // creates an empty array of length 10
 		};
-		~Array(){};
-		Array(unsigned int n)
+		~Array(){
+			delete [] _array;
+			std::cout << B_BLUEB << "Deleted array"  << B_END << std::endl;
+		};
+		Array(unsigned int n) : _len(n)
 		{
-
+			_array = new T[n];
+			std::cout << B_BLUEB << "Created an array of length: " << n << B_END << std::endl;
 		};
-		Array(const Array& obj); // deep copy
-		Array& operator=(const Array& obj); // deep copy
-		const unsigned int	size(void);
+		T	&operator[](int i)
+		{
+			if (i >= this->_len || i < 0)
+				throw OutOfBoundsException();
+			else
+				return _array[i];
+		}
+		Array(const Array& obj) : _len(obj._len)
+		{
+			std::cout << B_BLUEB << "Called copy constructor" << B_END << std::endl;
+			this->_array = new T[obj._len]; // make new array
+			for (int i = 0; i < obj._len; i++)
+				_array[i] = obj._array[i];
+		}; // deep copy
+		Array& operator=(const Array& obj)
+		{
+			std::cout << B_BLUEB << "Called assignation overload" << B_END << std::endl;
+			std::cout <<  B_BLUE << "delete old array" << B_END << std::endl;
+			delete [] this->_array; //delete old array
+			std::cout << B_BLUE << "create new array with proper obj._len: " << obj.getLen() << B_END << std::endl;
+			this->_array = new T[obj.getLen()]; // make new array
+			std::cout << B_BLUE << "fill array" << B_END << std::endl;
+			for (int i = 0; i < obj.getLen(); i++)
+				_array[i] = obj._array[i];
+			_len = obj.getLen();
+			return *this;
+		}; // deep copy
+		const unsigned int	size(void)
+		{
+			return this->_len;
+		}
+		const int		getLen(void) const
+		{
+			return this->_len;
+		}
+		class OutOfBoundsException : public std::exception
+		{
+			public:
+				const char *what() const throw() {
+					return "Index Out Of Bounds";
+				}
+		};
 };
 
 
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,24 +1,74 @@
 #include <iostream>
-#include <Array.hpp>
+#include "inc/Array.hpp"
+#include "inc/colours.hpp"
 
 #define MAX_VAL 750
 int main(int, char**)
 {
+    std::cout << B_GREEN << "Create an int array called numbers: Array<int> numbers(MAX_VAL)" << B_END << std::endl;
+    std::cout << B_GREEN << "Where MAX_VAL is 750" << B_END << std::endl;
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
+    std::cout << B_GREEN << "random seed for consistency" << B_END << std::endl;
     srand(time(NULL));
+    std::cout << B_GREEN << "Assigning values to the arrays" << B_END << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
+        std::cout << ".";
     }
+    std::cout << std::endl;
+    std::cout << B_GREEN << "Completed assigning values to array numbers and mirror" << B_END << std::endl;
     //SCOPE
     {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+        std::cout << B_GREEN << "Creating a scope:" << B_END << std::endl;
+        std::cout << B_GREEN << "Array<int> tmp; < should trigger default constructor" << B_END << std::endl;
+        Array<int> tmp;
+        std::cout << B_GREEN << "tmp = numbers; < should trigger the assignation operator" << B_END << std::endl;
+        tmp = numbers;
+        std::cout << B_GREEN << "Compare values in tmp to values in numbers, if a number doesn't match it prints an error" << B_END << std::endl;
+        for (int i = 0; i < MAX_VAL; i++)
+        {
+            if (tmp[i] != numbers[i])
+            {
+               std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+            std::cout << ".";
+        }
+        std::cout << B_GREEN << "\nSuccess!" << B_END << std::endl;
 
+        std::cout << B_GREEN << "Array<int> temp = numbers;" << B_END << std::endl;
+        Array<int> temp = numbers;        
+        std::cout << B_GREEN << "Compare values in temp to values in numbers, if a number doesn't match it prints an error" << B_END << std::endl;
+        for (int i = 0; i < MAX_VAL; i++)
+        {
+            if (temp[i] != numbers[i])
+            {
+               std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+            std::cout << ".";
+        }
+        std::cout << B_GREEN << "\nSuccess!" << B_END << std::endl;
+        
+        std::cout << B_GREEN << "Array<int> test(tmp); < should trigger the copy constructor" << B_END << std::endl;
+        Array<int> test(tmp);
+        std::cout << B_GREEN << "Compare values in test to values in numbers, if a number doesn't match it prints an error" << B_END << std::endl;
+        for (int i = 0; i < MAX_VAL; i++)
+        {
+            if (test[i] != numbers[i])
+            {
+               std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+            std::cout << ".";
+        }
+        std::cout << B_GREEN << "\nSuccess!" << B_END << std::endl;
+    }
+    std::cout << B_GREEN << "Compare values in mirror to values in numbers, if a number doesn't match it prints an error" << B_END << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         if (mirror[i] != numbers[i])
@@ -26,7 +76,10 @@ int main(int, char**)
             std::cerr << "didn't save the same value!!" << std::endl;
             return 1;
         }
+        std::cout << ".";
     }
+    std::cout << B_GREEN << "\nSuccess!" << B_END << std::endl;
+    std::cout << B_YELLOW << "Out of bounds testing: numbers[-2]:" << B_END << std::endl;
     try
     {
         numbers[-2] = 0;
@@ -35,6 +88,7 @@ int main(int, char**)
     {
         std::cerr << e.what() << '\n';
     }
+    std::cout << B_YELLOW << "max value testing numbers[MAX_VAL]:" << B_END << std::endl;
     try
     {
         numbers[MAX_VAL] = 0;
@@ -43,11 +97,12 @@ int main(int, char**)
     {
         std::cerr << e.what() << '\n';
     }
-
+    std::cout << B_RED << "Assigning random numbers to numbers array:" << B_END << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         numbers[i] = rand();
     }
+    std::cout << B_RED << "Complete!" << B_END << std::endl;
     delete [] mirror;//
     return 0;
 }
